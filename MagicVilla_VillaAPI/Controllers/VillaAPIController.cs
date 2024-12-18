108b7da8702bc8fc3e1ddd361b5b337734241434
Jaky1993 <125117605+Jaky1993@users.noreply.github.com>
2024-12-18 17:31:07 +0100

    HTTPGET and HTTPPOST initial

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,92 @@
+﻿using MagicVilla_VillaAPI.DATA;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    //[Route("api/[Controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        //controllerBase: contiene i metodi comuni per restituire tutti i dati e gli utenti che sono collegato ai
+        //controller in .net application
+
+        //se si una un'applicazione MVC deriviamo da Controller -> questo deriva da ControllerBase
+
+        //ENDPOINT
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            //ActionResult è l'implementazione dell'interfaccia IActionResult e ci permette di utilizzare qualsiasi tipo di
+            //ritorno che vogliamo
+            return Ok(VillaStore.villaList); 
+
+            //Ok object ritorna status Ok 200
+        }
+
+        [HttpGet("id:int", Name="GetVilla")]
+        [ProducesResponseType(StatusCodes.Status200OK)] //ritorna un villa model vuoto
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(200, Type = typeof(VillaDTO))] //ritorna un villa model vuoto
+        //[ProducesResponseType(404)]
+        //[ProducesResponseType(400)]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+
+            if(villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)] //ritorna un villa model vuoto
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO)
+        {
+            /*
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            */
+
+            if(VillaStore.villaList.FirstOrDefault(v => v.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("CustomError", "Villa already exists!");
+                //il primo parametro identifica il nome della chiave es: Name o ID ma in questo caso è una validazione
+                //Custom e non serve la chiave
+                return BadRequest(ModelState);
+            }
+
+            if(villaDTO == null)
+            {
+                return BadRequest(villaDTO);
+            }
+
+            if(villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(v => v.Id).FirstOrDefault().Id + 1;
+
+            VillaStore.villaList.Add(villaDTO);
+
+            return CreatedAtRoute("GetVilla", new { id = villaDTO.Id}, villaDTO);
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Models/DTO/VillaDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models.DTO
+{
+    public class VillaDTO
+    {
+        //DTO forniscono un wrapper tra le entity del database e ciò che viene esposto dalle API
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        //Se tolgo [ApiController] questi attributi spariscono, per tenerli devo fare un if(ModelState.IsValid)
+        public string Name { get; set; }
+    }
+}
