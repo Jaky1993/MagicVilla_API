8cadd09d000dfed1c067f9404e9b36825acc4227
Jaky1993 <125117605+Jaky1993@users.noreply.github.com>
2024-12-28 09:42:18 +0100

    Add MagicVilla_web

---
+++ MagicVilla_Web/Models/DTO/VillaUpdateDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_Web.Models.DTO
+{
+    public class VillaUpdateDTO
+    {
+        //DTO forniscono un wrapper tra le entity del database e ciò che viene esposto dalle API
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        //Se tolgo [ApiController] questi attributi spariscono, per tenerli devo fare un if(ModelState.IsValid)
+        public string Name { get; set; }
+        public string Details { get; set; }
+        [Required]
+        public double Rate { get; set; }
+        [Required]
+        public int Occupancy { get; set; }
+        [Required]
+        public int Sqft { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
+    }
+}
